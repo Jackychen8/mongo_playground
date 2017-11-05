d4c44d8803b2f44c9cf34236c2e5afc6c9ce01d5
Jacky Chen <sonderswift@gmail.com>
2017-11-05 12:24:49 -0800

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
 	email: {
@@ -72,6 +73,23 @@ UserSchema.statics.findByToken = function(token) {
 		'tokens.access': 'auth'
 	});
 }
+
+// mongoose middleware
+UserSchema.pre('save', function (next) {
+	var user = this;
+	if (user.isModified('password')) {
+		//user.password
+		bcrypt.genSalt(10, (err, salt) => {//10 rounds
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		console.log("password hasn't been modified");		
+		next();
+	}
+});
 
 const User = mongoose.model('User', UserSchema);
 
